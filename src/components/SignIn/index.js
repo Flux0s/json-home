efdf99d387316aca53571b15c6a2e88fe904f5b0
Jason <Flux0s@users.noreply.github.com>
2019-07-06 09:22:19 -0400

    Added .env schema and removed firebase vuln

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -2,53 +2,53 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { compose } from "recompose";
 
-import { withFirebase } from "../Firebase";
+// import { withFirebase } from "../Firebase";
 import * as ROUTES from "../../constants/routes";
 
 const SignIn = () => (
     <div>
         <h1>Sign In</h1>
-        <SignInGoogle />
+        {/* <SignInGoogle /> */}
     </div>
 );
 
-class SignInGoogleBase extends Component {
-    constructor(props) {
-        super(props);
+// class SignInGoogleBase extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = { error: null };
-    }
+//         this.state = { error: null };
+//     }
 
-    onSubmit = (event) => {
-        this.props.firebase
-            .doSignInWithGoogle()
-            .then((socialAuthUser) => {
-                this.setState({ error: null });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch((error) => {
-                this.setState({ error });
-            });
+//     onSubmit = (event) => {
+//         this.props.firebase
+//             .doSignInWithGoogle()
+//             .then((socialAuthUser) => {
+//                 this.setState({ error: null });
+//                 this.props.history.push(ROUTES.HOME);
+//             })
+//             .catch((error) => {
+//                 this.setState({ error });
+//             });
 
-        event.preventDefault();
-    };
+//         event.preventDefault();
+//     };
 
-    render() {
-        const { error } = this.state;
+//     render() {
+//         const { error } = this.state;
 
-        return (
-            <form onSubmit={this.onSubmit}>
-                <button type="submit">Sign In with Google</button>
+//         return (
+//             <form onSubmit={this.onSubmit}>
+//                 <button type="submit">Sign In with Google</button>
 
-                {error && <p>{error.message}</p>}
-            </form>
-        );
-    }
-}
+//                 {error && <p>{error.message}</p>}
+//             </form>
+//         );
+//     }
+// }
 
-const SignInGoogle = compose(
-    withRouter,
-    withFirebase
-)(SignInGoogleBase);
+// const SignInGoogle = compose(
+//     withRouter,
+//     withFirebase
+// )(SignInGoogleBase);
 
 export default SignIn;
